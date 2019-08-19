947b03b09128479c8f52caf036fecf9e130a68e7
Stephen Blais <stephen@s-blais.com>
2019-08-19 20:16:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,6 @@
 def square_array (numbers)
   squared_numbers = []
   numbers.each do |square_it|
-    #square_it = square_it*square_it
     squared_numbers.push (square_it*square_it)
   end
   squared_numbers
