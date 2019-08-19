bbdb89c073f3e5423237ac7f87dfda25afe2d115
Stephen Blais <stephen@s-blais.com>
2019-08-19 20:26:58 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -6,6 +6,8 @@ def square_array (numbers)
   squared_numbers
 end
 
+# Below seems to be a working version using .collect
+
 # def square_array (numbers)
   # numbers.collect do |square_it|
     # square_it*square_it
