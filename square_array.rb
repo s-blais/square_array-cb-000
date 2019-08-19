948a37bf5bd360a4b9d5de77e765fbcac1070577
Stephen Blais <stephen@s-blais.com>
2019-08-19 20:27:06 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
-def square_array(array)
-  # your code here
+def square_array (numbers)
+  squared_numbers = []
+  numbers.each do |square_it|
+    squared_numbers.push (square_it*square_it)
+  end
+  squared_numbers
 end
+
+# Below seems to be a working version using .collect
+
+# def square_array (numbers)
+  # numbers.collect do |square_it|
+    # square_it*square_it
+  # end
+# end
