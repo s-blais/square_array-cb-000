61b3b4bd11815729b994cb50ec6f2e3eef71a81b
Stephen Blais <stephen@s-blais.com>
2019-08-19 20:26:23 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,13 @@
-#def square_array (numbers)
-  #squared_numbers = []
-  #numbers.each do |square_it|
-    #squared_numbers.push (square_it*square_it)
-  #end
-  #squared_numbers
-#end
-
 def square_array (numbers)
-  #squared_numbers = []
-  numbers.collect do |square_it|
-    square_it*square_it
+  squared_numbers = []
+  numbers.each do |square_it|
+    squared_numbers.push (square_it*square_it)
   end
+  squared_numbers
 end
+
+# def square_array (numbers)
+  # numbers.collect do |square_it|
+    # square_it*square_it
+  # end
+# end
